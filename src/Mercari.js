2dae5eee3e67395818a031e29b86e743d3a0b5a0
Takashi Endo <endotakashi1992@gmail.com>
2017-08-04 14:26:26 +0900

    set mercari url

---
+++ src/Mercari.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function(){
+  return(
+    <div>
+      <head>
+      	<meta charset="utf-8"/>
+      	<meta name="keywords" content=",,,"/>
+      	<meta name="description" content=""/>
+      	<meta name="viewport" content="width=device-width"/>
+      	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      	<title>sp</title>
+      	<link href="/css/reset.css" rel="stylesheet" type="text/css"/>
+      	<link href="/css/style.css" rel="stylesheet" type="text/css"/>
+      	<link href="/css/sp.css" rel="stylesheet" type="text/css"/>
+      	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+      </head>
+      <body>
+      <div id="wrapper">
+
+      	<div className="bgmerlari">
+      		<div className="nice-btnBox">
+      			<span className="nice_check"><i className="fa fa-check-circle" aria-hidden="true"></i></span>
+      			<p className="nice-text">N-LICENCE</p>
+      		</div>
+      	</div>
+      </div>
+
+      	<script src="js/jquery-2.1.4.min.js"></script>
+  </body>
+</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Mercari from './Mercari';
 import { Router, Route, browserHistory } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(    <Router history={browserHistory}>
         <Route path="/" component={App} />
+        <Route path="/mercari" component={Mercari} />
     </Router>, document.getElementById('root'));
 registerServiceWorker();
